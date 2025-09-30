319cdeec3356cec1fa79d34d9858282751831e7b
Ahmed Sekhni <20105853@mail.wit.ie>
2025-09-30 12:27:19 +0100

    Test

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,18 +1,33 @@
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+
+    const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
+   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-      </Task>
-      <Task title="Tidy" deadline="Today" />
-      
+       {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+    />
+  ))} 
+
     </div>
   );
+
+
+
 }
 
 export default App;
